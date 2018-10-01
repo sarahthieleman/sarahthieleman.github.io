21dbaa8e1d1b8ad18daea9def372a0056431a0ff
sarahthieleman <42797978+sarahthieleman@users.noreply.github.com>
2018-10-02 00:03:04 +0200

    Create loops.js

---
+++ programmingbasics/loops/loops.js
@@ -0,0 +1,18 @@
+// Opdracht 1: Maak een while loop die kijkt of een in een reeks getallen deelbaar zijn door 4.
+
+var x = 1;
+
+while (x <= 30) {
+    if (x % 4 == 0){
+	console.log(x + " dit nummer is deelbaar door 4");
+    }else {
+	console.log(x + " dit nummer is niet deelbaar door 4");
+    }
+    x ++
+}
+
+
+
+// Opdracht 2: Schrijf een berekening van een Fibonacci reeks met while loop.
+
+
