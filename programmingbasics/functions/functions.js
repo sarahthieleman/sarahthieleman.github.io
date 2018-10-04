1d34eddd2cf643f79988b1d5121a49a05f50c6b3
sarahthieleman <42797978+sarahthieleman@users.noreply.github.com>
2018-10-04 22:08:56 +0200

    Update functions.js

--- programmingbasics/functions/functions.js
+++ programmingbasics/functions/functions.js
@@ -1,36 +1,32 @@
-// Opdracht 1: Zet de uitwerking van de Fibonacci getallenreeks uit de vorige les om in een functie.
-
+/*
+Opdracht 1: Zet de uitwerking van de Fibonacci getallenreeks uit de vorige les om in een functie.
+*/
 
 //function name is fibonacci
 function fibonacci(){
 
-// variables
-var a = 0;
-var b = 1;
-
-// while loop
-while (a <= 60) 
-{
-    a = a + b
-    b = a - b
-    console.log (a)
-}
-}
-
-// print function named fibonacci
-fibonacci()
-
-
-// Opdracht 2:
+    // variables
+    var a = 0;
+    var b = 1;
+    
+    // while loop
+    while (a <= 60) 
+    {
+        a = a + b
+        b = a - b
+        console.log (a)
+    }
+    }
+    
+    // print function named fibonacci
+    fibonacci()
 
 
 
+/*
+Opdracht 2: Schrijf een functie countdown die van 10 - 1 aftelt. 
+Geef aan de functie een jaartal parameter mee die als hij bij ‘0’ is zegt ‘Happy {jaartal}’.
+*/
 
 
 
-
-
-
-2.	Schrijf een functie countdown die van 10 - 1 aftelt (dit wordt ook in de console getoond). Geef aan de functie een jaartal parameter mee die als hij bij ‘0’ is zegt ‘Happy {jaartal}’.
-3.	Schrijf een functie expressie en declaratie waarmee je aantoont dat een expressie en een declaratie op een andere manier met hoisting omgaan. (als je deze vraag niet snapt dan moet je de videoreeks uit udacity kijken. Daar wordt hoisting goed uitgelegd).
-4.	[facultatief; alleen voor studenten die extra verdieping zoeken] Installeer TypeScript (https://www.typescriptlang.org/) en maak opdracht 1, 2 en 3 met behulp van Typescript. Let er op dat je nu ook voor alle variabelen een type moet declareren. 
